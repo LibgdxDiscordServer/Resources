634953d6b87fcedbf162ea80a20f734e2a027f85
John <libgdxdiscord@gmail.com>
2018-02-26 22:23:22 -0800

    i

--- js/entry.js
+++ js/entry.js
@@ -42,7 +42,7 @@ function createCardGroup(result, week){
         numberOfCardsInRow++;
 
         //create column
-        deck += '<div class="col-sm">';
+        deck += '<div class="col-sm-4">';
         //create card
         deck += createCard(title, author, description, sourceLink, playOnlineLink, jarLink, playStoreLink, previewImage);
         //end column
